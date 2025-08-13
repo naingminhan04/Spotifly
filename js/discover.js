9333ae907b0bd9f169b2c617f90a69d2b9d715eb
elliot3008 <naingminhan@icloud.com>
2025-08-14 00:04:16 +0630

    Test Deployment for play/pause function

--- js/discover.js
+++ js/discover.js
@@ -1,90 +1,115 @@
 addDiscover();
 
 function addDiscover() {
-    const displaySong = document.querySelector('#discover');
-    songs.forEach(song => {
-        const card = createSongCard(song);
-        displaySong.appendChild(card);
-    });
+  const displaySong = document.querySelector("#discover");
+  songs.forEach((song) => {
+    const card = createSongCard(song);
+    displaySong.appendChild(card);
+  });
 }
 
 function createSongCard(song) {
-    const col = document.createElement('div');
-    col.classList = 'col-12 col-sm-6 col-md-4 col-lg-3'
+  const col = document.createElement("div");
+  col.classList = "col-12 col-sm-6 col-md-4 col-lg-3";
 
-    const card = document.createElement('div');
-    card.classList = 'card h-100 d-flex flex-column';
+  const card = document.createElement("div");
+  card.classList = "card h-100 d-flex flex-column";
 
-    const img = document.createElement('img');
-    img.classList.add('album-art','card-img-top');
-    img.src = song.img;
-    
-    const cardBody = document.createElement('div');
-    cardBody.classList = 'card-body d-flex flex-column';
+  const img = document.createElement("img");
+  img.classList.add("album-art", "card-img-top");
+  img.src = song.img;
 
-    const title = document.createElement('h5');
-    title.classList = 'card-title';
-    title.textContent = song.title;
+  const cardBody = document.createElement("div");
+  cardBody.classList = "card-body d-flex flex-column";
 
-    const artist = document.createElement('p');
-    artist.classList = 'card-text text-muted';
-    artist.textContent = song.artist;
+  const title = document.createElement("h5");
+  title.classList = "card-title";
+  title.textContent = song.title;
 
-    const btnContainer = document.createElement('div');
-    btnContainer.classList = 'd-flex justify-content-between mt-auto';
+  const artist = document.createElement("p");
+  artist.classList = "card-text text-muted";
+  artist.textContent = song.artist;
 
-    const playBtn = document.createElement('button');
-    playBtn.classList = 'btn btn-dark btn-sm';
-    playBtn.innerHTML = '<i class="fa-solid fa-play"> Play</i>';
-    playBtn.addEventListener('click', ()=> playSong(song,playBtn))
-    
-    const addBtn = document.createElement('button');
-    addBtn.classList = 'btn btn-outline-secondary btn-sm';
-    addBtn.innerHTML = '<i class="fa-solid fa-plus"> <span class="d-lg-none">Add</span></i>';
-    addBtn.addEventListener('click', () => addSong(song))
+  const btnContainer = document.createElement("div");
+  btnContainer.classList = "d-flex justify-content-between mt-auto";
 
-    btnContainer.appendChild(playBtn);
-    btnContainer.appendChild(addBtn);
+  const playBtn = document.createElement("button");
+  playBtn.classList = "btn btn-dark btn-sm";
+  playBtn.innerHTML = '<i class="fa-solid fa-play"> Play</i>';
+  playBtn.addEventListener("click", () => playSong(song, playBtn));
 
-    cardBody.appendChild(title);
-    cardBody.appendChild(artist);
-    cardBody.appendChild(btnContainer);
+  const addBtn = document.createElement("button");
+  addBtn.classList = "btn btn-outline-secondary btn-sm";
+  addBtn.innerHTML =
+    '<i class="fa-solid fa-plus"> <span class="d-lg-none">Add</span></i>';
+  addBtn.addEventListener("click", () => addSong(song));
 
-    card.appendChild(img);
-    card.appendChild(cardBody);
+  btnContainer.appendChild(playBtn);
+  btnContainer.appendChild(addBtn);
 
-    col.appendChild(card);
+  cardBody.appendChild(title);
+  cardBody.appendChild(artist);
+  cardBody.appendChild(btnContainer);
 
-    return col;
+  card.appendChild(img);
+  card.appendChild(cardBody);
+
+  col.appendChild(card);
+
+  return col;
 }
 
 let currentButton = null;
+let currentSong = null;
 
-function playSong (song,playBtn) {
-    const audioElement = document.querySelector('audio');
-    const sourceElement = audioElement.querySelector('source');
+const footerImg = document.querySelector("#footer-img");
+const footerArtist = document.querySelector("#footer-artist");
+const footerSong = document.querySelector("#footer-song");
+const footPlayBtn = document.querySelector("#foot-play-btn");
 
-    sourceElement.src = song.audio;
+const volume = document.querySelector("#volume");
 
-    if(currentButton === playBtn){
-        if(audioElement.paused) {
-            audioElement.play();
-            playBtn.innerHTML = '<i class="fa-solid fa-pause"> Pause</i>'
-        } else {
-            audioElement.pause();
-            playBtn.innerHTML = '<i class="fa-solid fa-play">Play</i>'
-        }
-        return;
+function playSong(song, playBtn) {
+  const audioElement = document.querySelector("audio");
+  const sourceElement = audioElement.querySelector("source");
+
+  sourceElement.src = song.audio;
+
+  if (currentButton === playBtn) {
+    if (audioElement.paused) {
+      audioElement.play();
+      playBtn.innerHTML = '<i class="fa-solid fa-pause"> Pause</i>';
+      footPlayBtn.classList.replace("fa-play", "fa-pause");
+    } else {
+      audioElement.pause();
+      playBtn.innerHTML = '<i class="fa-solid fa-play">Play</i>';
+      footPlayBtn.classList.replace("fa-pause", "fa-play");
     }
+    return;
+  }
 
-    if(currentButton) {
-        currentButton.innerHTML = '<i class="fa-solid fa-play">Play</i>';
-    }
+  if (currentButton) {
+    currentButton.innerHTML = '<i class="fa-solid fa-play">Play</i>';
+  }
 
-    audioElement.load();
-    audioElement.play();
+  audioElement.load();
+  audioElement.play();
 
-    playBtn.innerHTML = '<i class="fa-solid fa-pause"> Pause</i>'
+  playBtn.innerHTML = '<i class="fa-solid fa-pause"> Pause</i>';
+  footPlayBtn.classList.remove("fa-play");
+  footPlayBtn.classList.add("fa-pause");
 
-    currentButton = playBtn;
+  currentButton = playBtn;
+  currentSong = song;
+
+  footerImg.src = currentSong.img;
+  footerImg.classList.replace("d-none", "d-flex");
+  footerArtist.textContent = currentSong.artist;
+  footerSong.textContent = currentSong.title;
+  volume.classList.add("d-lg-flex");
 }
+
+footPlayBtn.addEventListener("click", () => {
+  if (!currentButton || !currentSong) return;
+  playSong(currentSong, currentButton);
+});
